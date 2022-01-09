b2c46eab5fb798b57048c962de9dbf6ad3cd989a
Lennard Metson <74358730+lenmetson@users.noreply.github.com>
2022-01-09 21:05:38 +0000

    removed positions

--- scripts/main_script.R
+++ scripts/main_script.R
@@ -10,5 +10,16 @@ download.file(url, here("data_raw", "rawdata.csv"))
 rm(url)
 
 # We then need to clean the data. In order to keep the main script a reasonable length, we will source a separate script to do this.
-# The following script will add a catagory for which parliamnet a speech is in.
+# The following script will add a catagory for which parliament is in, remove duplicate speeches and create a list of MSPs
 source(here("scripts", "clean_data.R"))
+
+# Next we should remove MSPs, with positions because we are only interested in backbenchers
+speeches <- speeches %>% filter(office_held == "N/A" | # 230846obvs of 17vars
+                                 office_held == "." |
+                                 office_held == "n/a" |
+                                 is.na(office_held))
+
+# Make a list of MSPs who remain in the sample 
+MSPs <- speeches %>%
+  select(parl_id, name, gender, wikidataid, constituency, region, msp_type, office_held, parly) %>%
+  unique()
