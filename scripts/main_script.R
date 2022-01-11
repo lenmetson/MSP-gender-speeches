1e708ec0b1d766281879e58b304dca192a888aae
Lennard Metson <74358730+lenmetson@users.noreply.github.com>
2022-01-11 18:52:50 +0000

    finished analysis

--- scripts/main_script.R
+++ scripts/main_script.R
@@ -3,6 +3,7 @@
 # First we need to install/ load packages
 if(!require("here"))install.packages("here")
 if(!require("tidyverse"))install.packages("tidyverse")
+if(!require("sylcount"))install.packages("sylcount")
 
 # Next we need to download raw data. ONLY RUN LINE ONCE. The following line will download a file from Harvard Dataverse to the data_raw folder on your local disk
 #url <- "https://dataverse.harvard.edu/api/access/datafile/4432885"
@@ -18,6 +19,7 @@ speeches <- speeches %>% filter(office_held == "N/A" | # 230846 obs. of 17 vars.
                                  office_held == "." |
                                  office_held == "n/a" |
                                  is.na(office_held))
+speeches$office_held <- NULL # remove variable
 
 # Make a list of MSPs who remain in the sample
 MSPs <- speeches %>%
@@ -41,3 +43,49 @@ proportion_female_speeches <- female_speeches/nrow(speeches)
 female_speech_per_msp <- female_speeches/female_msps
 
 male_speech_per_msp <- male_speeches/male_msps
+
+# Count syllables 
+speeches$syls <- sylcount(speeches$speech)
+speeches$syls <- sapply(speeches$syls, sum)
+
+# Number of sylables back-bench female MSPs are speeching per day  
+
+syls_per_day_women <- speeches %>%
+  filter(gender == "F") %>% 
+  group_by(date) %>%
+  summarise(syls = sum(syls))
+
+syls_per_day_f_plot <- 
+  ggplot(syls_per_day_women, aes(x=date, y=syls)) + geom_smooth(method = loess, se=FALSE, color ="black")
+
+syls_per_day_f_plot # view plot
+
+# Number of syllables back-bench male MSPs are speeching per day  
+
+syls_per_day_men <- speeches %>%
+  filter(gender == "M") %>% 
+  group_by(date) %>%
+  summarise(syls = sum(syls))
+
+syls_per_day_m_plot <- 
+  ggplot(syls_per_day_men, aes(x=date, y=syls)) + geom_smooth(method = loess, se=FALSE, color ="black")
+
+syls_per_day_m_plot # view plot
+
+# Proportion of sylables by women per day 
+
+syls_per_day_all <- speeches %>%
+  group_by(date) %>%
+  summarise(syls_all = sum(syls))
+
+proportion_women <- full_join(syls_per_day_all, syls_per_day_women, by = "date") # join the summary dataframe for all speeches and women speakers.
+proportion_women[is.na(proportion_women)] <- 0 # days where women didn't speak any sylables are still kept becayse of full_join but there is an NA, so we need to convert NAs to 0% 
+proportion_women$prop_f <- proportion_women$syls/proportion_women$syls_all
+
+proportion_women_plot <- 
+  ggplot(proportion_women, aes(x=date, y=prop_f)) + 
+  geom_point() +
+  geom_smooth(method = loess, se=FALSE, color ="blue") + theme_bw()
+
+proportion_women_plot # view plot
+
